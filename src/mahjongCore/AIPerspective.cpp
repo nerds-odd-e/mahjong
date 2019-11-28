4ce9e71bbac12892ca72e8c3cf88eeb7045322c8
Natalia <natalia.juradoes@gmail.com>
2019-11-28 20:12:27 +0800

    Remove dead code

--- src/mahjongCore/AIPerspective.cpp
+++ src/mahjongCore/AIPerspective.cpp
@@ -20,10 +20,10 @@ Tile AIPerspective::whichToDiscard() {
 	int index_to_throw = 0;
 	Tile holdings[ai_max_holding_count + 1];
 	Tile tiles[ai_max_holding_count + 1];
-	int tile_count = player->getHoldings(holdings, settings_.GetHandSize());
+	int tile_count = player->getHoldings(holdings, ai_max_holding_count);
 	holdings[tile_count] = player->getCurrentTileAtHand();
 	for (i = 0; i < tile_count + 1; i++) {
-		player->getHoldings(tiles, settings_.GetHandSize());
+		player->getHoldings(tiles, ai_max_holding_count);
 		tiles[i] = player->getCurrentTileAtHand();
 		int score = evaluator->evaluate_array(TileArray(tiles, tile_count));
 		if (score > max) {
